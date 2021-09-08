adf4e60408475a6f8bdbbc7b8cd3047b1aba9a82
JeremiasFuentes <39131303+JeremiasFuentes@users.noreply.github.com>
2021-09-08 18:14:02 -0300

    añadido proyecto creado con spring boot

---
+++ Backend/src/main/kotlin/edu/desapp/backend/grupoD/GrupoDApplication.kt
@@ -0,0 +1,11 @@
+package edu.desapp.backend.grupoD
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrupoDApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrupoDApplication>(*args)
+}
---
+++ Backend/src/test/kotlin/edu/desapp/backend/grupoD/GrupoDApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.desapp.backend.grupoD
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrupoDApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
